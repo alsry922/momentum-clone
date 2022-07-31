41f2caa878c30f26bd64d35174f3a53a5549b0a1
alsry922 <alsry922@gmail.com>
2022-07-31 17:08:39 +0900

    login

---
+++ src/login.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const USERNAME_KEY = "username";
+const CLASSNAME_HIDDEN = "hidden";
+
+const $loginForm = document.querySelector("#login-form");
+const $nameInput = $loginForm.querySelector(".name-input");
+const $greeting = document.querySelector(".greeting");
+
+setGreeting();
+
+function onSubmitLogin(event) {
+  event.preventDefault();
+  const inputData = $nameInput.value;
+  $nameInput.value = "";
+  localStorage.setItem(USERNAME_KEY, inputData);
+  setGreeting();
+  $loginForm.classList.add(CLASSNAME_HIDDEN);
+}
+
+function setGreeting() {
+  const userName = localStorage.getItem(USERNAME_KEY);
+  if (userName === null) {
+    $greeting.textContent = `Hello my super star!`;
+    $loginForm.classList.remove(CLASSNAME_HIDDEN);
+  } else {
+    $greeting.textContent = `Hello ${userName}!`;
+    $loginForm.classList.add(CLASSNAME_HIDDEN);
+  }
+}
+
+$loginForm.addEventListener("submit", onSubmitLogin);
